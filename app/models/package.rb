7d07003ab21353085ce3884d806c405b763634aa
nick <nicholas.newberry@rcgtconsulting.com>
2022-11-21 12:26:26 -0500

    validate associated

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,5 @@
 class Package < ApplicationRecord
   belongs_to :truck, optional: true
+
+  validates_associated :truck
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,7 +1,19 @@
 class Truck < ApplicationRecord
   has_many :packages
 
-  def load(package)
+  validates :cargo_size, comparison: { less_than_or_equal_to: :capacity }
+
+  def load_cargo(package)
     package.update!(truck: self)
+  end
+
+  def cargo_size
+    packages.sum(&:size)
+  end
+
+  private
+
+  def capacity_not_exceeded
+    cargo_size <= capacity
   end
 end
