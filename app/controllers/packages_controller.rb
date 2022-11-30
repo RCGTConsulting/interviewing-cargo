a15f7028ab7d0f6c000c22094f9b87ddf56da8fe
nick <nicholas.newberry@rcgtconsulting.com>
2022-11-30 10:15:02 -0500

    controller problem 7

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,10 @@
 class PackagesController < ApplicationController
   def show
     @package = Package.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @package }
+    end
   end
 end
